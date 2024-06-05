b2f784af330ba7c87cf3d028f8099598e3648437
loulou <loulou.tobias@gmail.com>
2024-06-05 09:19:58 +0200

    suite

--- src/main/java/org/example/UtilisateurTab.java
+++ src/main/java/org/example/UtilisateurTab.java
@@ -1,5 +1,29 @@
 package org.example;
 
-public class UtilisateurTab {
+import java.util.regex.Pattern;
 
+public class UtilisateurTab {
+    private String prenom;
+    private String nom;
+    private String email;
+
+    public UtilisateurTab(String prenom, String nom, String email) throws IllegalArgumentException {
+        if (!isValidEmail(email)) {
+            throw new IllegalArgumentException("Email non valide");
+        }
+        this.prenom = prenom;
+        this.nom = nom;
+        this.email = email;
+    }
+
+    public static boolean isValidEmail(String email) {
+        String emailRegex = "^[\\w.-]+@[\\w.-]+\\.[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        return pattern.matcher(email).matches();
+    }
+
+    @Override
+    public String toString() {
+        return "Prénom: " + prenom + ", Nom: " + nom + ", Email: " + email;
+    }
 }
