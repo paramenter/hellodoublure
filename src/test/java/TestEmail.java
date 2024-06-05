245a9ee571e019954002bc430147e22619fec107
grelin1u <kevin.grelin5@etu.univ-lorraine.fr>
2024-06-05 06:38:38 +0200

    TP2

---
+++ src/test/java/TestEmail.java
@@ -0,0 +1,13 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class TestEmail {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Veuillez saisir votre mail: ");
+            String email = sc.next("[\\w.-]+@[\\w.-]+\\.[a-z]{2,}");
+            System.out.println(email + " : Email valide !");
+        } catch (InputMismatchException e) {
+            System.out.println("Email non valide");
+        }
+    }
+}
